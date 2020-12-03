885379d91bb62c9f8b3f2a35ac7793256a1c11ce
heymonicakay <hey.monicakay@gmail.com>
2020-12-03 14:47:23 -0600

    fixed average product rating to only assign an avg rating when there is at least one rating. get all products and get single product now functional.

---
+++ bangazon.session.sql
@@ -0,0 +1,36 @@
+-- SELECT * FROM bangazonapi_product
+
+-- SELECT * FROM bangazonapi_productrating;
+
+-- SELECT * FROM bangazonapi_rating;
+
+
+-- SELECT
+--     *
+-- FROM bangazonapi_recommendation
+
+
+-- SELECT
+--     *
+-- FROM bangazonapi_favorite
+
+-- SELECT
+--     *
+-- FROM bangazonapi_order
+
+
+-- SELECT
+--     p.name,
+--     p.price,
+--     p.description,
+--     p.id
+-- FROM `bangazonapi_product` p
+-- WHERE p.price >= 1500.00
+
+
+SELECT
+    *
+FROM bangazonapi_productrating
+
+-- try to get ratings
+if no ratings, return
